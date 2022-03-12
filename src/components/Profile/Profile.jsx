2ac373de909b960e4bf2c3d1193119c8208944c7
versace99 <charyyevserdar27@gmail.com>
2022-03-12 21:14:45 +0300

    second commit

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import addPost from "../../redux/state";
 
 const Profile = (props) => {
-
     return <div>
         <ProfileInfo/>
-        <MyPosts posts={props.state.posts}/>
+        <MyPosts posts={props.profilePage.posts}
+                 newPostText={props.profilePage.newPostText}
+                 updateNewPostText={props.updateNewPostText}
+                 addPost={addPost}/>
     </div>
 
 
